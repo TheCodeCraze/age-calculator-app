f8ebf298c8ff9944a878fe9908ab5b77f31e761c
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-05-12 07:27:23 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import styles from "../styles/App.module.css";
+
+const App = () => {
+  return <main className={styles.main}></main>;
+};
+
+export default App;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/Calculator.module.css";
+
+const Calculator = () => {
+  return <div className={styles.calculator}>Calculator</div>;
+};
+
+export default Calculator;
+
+// Day
+// DD
+
+// Month
+// MM
+
+// Year
+// YYYY
+
+// -- years
+// -- months
+// -- days
