59754f49488db50d123fe854b944a620a14a9b51
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-05-13 12:54:11 +0530

    Added validations to form

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import styles from "../styles/Calculator.module.css";
+import { useFormik } from "formik";
+import { ageSchema } from "../schema/ageSchema";
 
 const Calculator = () => {
+  const [result, setResult] = useState({
+    years: "- -",
+    months: "- -",
+    days: "- -",
+  });
+  const handleSubmit = (values) => {};
+  const formik = useFormik({
+    initialValues: {
+      day: "",
+      month: "",
+      year: "",
+    },
+    validationSchema: ageSchema,
+    onSubmit: handleSubmit,
+  });
+
   return (
     <div className={styles.calculator}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={formik.handleSubmit}>
         <div className={styles.inputs}>
-          <div className={styles.group}>
+          <div
+            className={`${styles.group} ${
+              formik.touched.day && formik.errors.day && styles.error
+            }`}
+          >
             <label htmlFor="day" className={styles.label}>
               Day
             </label>
@@ -15,9 +38,17 @@ const Calculator = () => {
               type="number"
               className={styles.input}
               placeholder="DD"
+              value={formik.values.day}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            <p className={styles.message}>{formik.errors.day}</p>
           </div>
-          <div className={styles.group}>
+          <div
+            className={`${styles.group} ${
+              formik.touched.month && formik.errors.month && styles.error
+            }`}
+          >
             <label htmlFor="month" className={styles.label}>
               Month
             </label>
@@ -27,9 +58,17 @@ const Calculator = () => {
               type="number"
               className={styles.input}
               placeholder="MM"
+              value={formik.values.month}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            <p className={styles.message}>{formik.errors.month}</p>
           </div>
-          <div className={styles.group}>
+          <div
+            className={`${styles.group} ${
+              formik.touched.year && formik.errors.year && styles.error
+            }`}
+          >
             <label htmlFor="year" className={styles.label}>
               Year
             </label>
@@ -39,7 +78,11 @@ const Calculator = () => {
               type="number"
               className={styles.input}
               placeholder="YYYY"
+              value={formik.values.year}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            <p className={styles.message}>{formik.errors.year}</p>
           </div>
         </div>
         <div className={styles.seperator}>
@@ -55,15 +98,15 @@ const Calculator = () => {
       </form>
       <div className={styles.result}>
         <div className={styles.years}>
-          <p className={styles.value}>- -</p>
+          <p className={styles.value}>{result.years}</p>
           <p className={styles.text}>years</p>
         </div>
         <div className={styles.months}>
-          <p className={styles.value}>- -</p>
+          <p className={styles.value}>{result.months}</p>
           <p className={styles.text}>months</p>
         </div>
         <div className={styles.days}>
-          <p className={styles.value}>- -</p>
+          <p className={styles.value}>{result.days}</p>
           <p className={styles.text}>days</p>
         </div>
       </div>
---
+++ src/schema/ageSchema.js
@@ -0,0 +1,68 @@
+import * as Yup from "yup";
+
+const now = new Date();
+const currentDay = now.getDate();
+const currentMonth = now.getMonth() + 1;
+const currentYear = now.getFullYear();
+
+export const ageSchema = Yup.object().shape({
+  day: Yup.number()
+    .required("This field is required")
+    .test("Must be a valid day", "Must be a valid day", (day, context) => {
+      const { month, year } = context.parent;
+      if (day >= 1 && day <= 31) {
+        if (!month || !year) {
+          return true;
+        } else {
+          if (
+            year === currentYear &&
+            month === currentMonth &&
+            day <= currentDay
+          ) {
+            return true;
+          } else if (
+            year === currentYear &&
+            month === currentMonth &&
+            day > currentDay
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+        }
+      } else {
+        return false;
+      }
+    }),
+  month: Yup.number()
+    .required("This field is required")
+    .test(
+      "Must be a valid month",
+      "Must be a valid month",
+      (month, context) => {
+        const { year } = context.parent;
+        if (month >= 1 && month <= 12) {
+          if (!year) {
+            return true;
+          } else {
+            if (year === currentYear && month <= currentMonth) {
+              return true;
+            } else if (year === currentYear && month > currentMonth) {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        } else {
+          return false;
+        }
+      }
+    ),
+  year: Yup.number()
+    .required("This field is required")
+    .test(
+      "Must be in the past",
+      "Must be in the past",
+      (year) => year <= currentYear
+    ),
+});
